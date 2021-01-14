07648a5ef9e290612e7fcaf9a6b17f06f41252fb
Israel <idias@infofisco.com.br>
2021-01-14 15:59:30 -0300

    Projeto teste selenium

---
+++ src/app/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private static IWebDriver driver = null;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ChromeDriverService chromeService = ChromeDriverService.CreateDefaultService(string.Concat(Directory.GetCurrentDirectory(), @"\wwwroot\Resources"));
+            chromeService.HideCommandPromptWindow = true;
+            chromeService.SuppressInitialDiagnosticInformation = true;
+
+            OpenQA.Selenium.Chrome.ChromeOptions chromeOptions = new OpenQA.Selenium.Chrome.ChromeOptions();
+            chromeOptions.AddArguments(string.Concat("--app=", @"http://nfce.fazenda.mg.gov.br/portalnfce/sistema/consultaarg.xhtml"));
+
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver(chromeService, chromeOptions);
+
+            if (driver == null)
+                    throw new Exception("WebDriver do Selenium não definido nas configurações");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
